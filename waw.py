8194ebcec6280713fcaeaa1a74c7d0a2a598f7be
fi6uh <fi6uh@protonmail.com>
2024-05-03 20:09:19 -0500

    add URL argument support and debugger

--- waw.py
+++ waw.py
@@ -41,7 +41,7 @@ class API:
         self.config = self.read()
         token = None
         if "auth" in self.config.keys():
-            token = self.config["auth"]
+            token = os.environ.get(self.config["auth"])
         host = self.config["host"]
         headers = Headers(token)
         self.client = WWWHost(headers.headers, host)
@@ -53,19 +53,59 @@ class API:
                 data = json.load(fh)
         return data
 
-    def call(self, function, parameters={}):
+    def call(self, function, parameters={}, url_args={}):
         if function in self.config["endpoints"].keys():
             method = self.config["endpoints"][function]["method"]
             endpoint = self.config["endpoints"][function]["endpoint"]
+            if "{" in endpoint and "}" in endpoint:
+                endpoint = endpoint.format(**url_args)
             response = self.client.simple_req(method, endpoint, parameters)
             return response
 
 
+class Debug:
+    def __init__(self, response):
+        self.response = response
+        self.request = response.request
+
+    def get_obj_data(self, obj):
+        cookies = {}
+        if "cookies" in obj.__dict__.keys():
+            cookies = obj.cookies
+        else:
+            cookies = obj._cookies
+        cookies = cookies.get_dict()
+        data = {
+            "url" : obj.url,
+            "headers" : dict(obj.headers),
+            "cookies" : dict(cookies)
+        }
+        return data
+
+    def __str__(self):
+        code = self.response.status_code
+        request = self.get_obj_data(self.request)
+        response = self.get_obj_data(self.response)
+        debug = {
+            "request": request,
+            "response": response,
+            "status": code
+        }
+        return json.dumps(debug, indent=2)
+
+
 def main():
-    test = "examples/test.json"
+    test = "examples/so.json"
     api = API(test)
-    data = api.call("home")
-    print(data.text[:100])
+
+    first_so_question = {
+        "qid": 4,
+        "title": "how-to-convert-decimal-to-double-in-c"
+    }
+    data = api.call("questions", url_args=first_so_question)
+
+    debugger = Debug(data)
+    print(debugger)
 
 
 if __name__ == "__main__":
